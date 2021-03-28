577613a897b446af98ea1657e0bc33dd587aa03d
Manav Malik <manavmalik36@gmail.com>
2021-03-28 09:29:46 -0400

    updated how enchantments work

--- src/main/java/com/manav/crystal/items/DarkSword.java
+++ src/main/java/com/manav/crystal/items/DarkSword.java
@@ -68,7 +68,7 @@ public class DarkSword extends SwordItem {
 	@Override
 	public void onCreated(ItemStack stack, World worldIn, PlayerEntity playerIn) {
 		
-		stack.addEnchantment(Enchantment.getEnchantmentByID(16), 10); // Looting X
+		stack.addEnchantment(Enchantment.getEnchantmentByLocation('looting'), 10);
 		playerIn.setHealth(playerIn.getMaxHealth());
 		worldIn.setRainStrength(10.0f);
 		worldIn.setThunderStrength(10.0f);
